d32db0e4c6884c397d5e3f170285d1f0637318d3
otiai10 <otiai10@gmail.com>
2014-08-12 08:08:59 +0900

    Add test conf accessor

--- common/map.go
+++ common/map.go
@@ -1,6 +1,12 @@
 package common
 
 var (
+	redis_test = map[int]map[string]string{
+		0: map[string]string{
+			"host": "localhost",
+			"port": "6379",
+		},
+    }
 	redis = map[int]map[string]string{
 		0: map[string]string{
 			"host": "localhost",
