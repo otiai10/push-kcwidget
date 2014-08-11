f76e0efbec57a1af67a5cea7b9d451dfbdb4a394
otiai10 <otiai10@gmail.com>
2014-08-12 08:14:19 +0900

    Add tests for common

---
+++ common/all_test.go
@@ -0,0 +1,11 @@
+package common_test
+
+import "push-kcwidget/common"
+import "testing"
+import . "github.com/otiai10/mint"
+
+func TestGetRedisHostAndPort(t *testing.T) {
+	tHost, tPort := common.GetRedisHostAndPort(-1)
+	Expect(t, tHost).ToBe("localhost")
+	Expect(t, tPort).ToBe("6379")
+}
