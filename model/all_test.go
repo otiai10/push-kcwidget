501dbd97c1a17cbbe9963bb670e9d702b9dddd26
otiai10 <otiai10@gmail.com>
2014-08-17 22:57:50 +0900

    Add tests for model

--- model/all_test.go
+++ model/all_test.go
@@ -9,12 +9,11 @@ import "time"
 func init() {
 	common.SetPrefix("test.")
 	model.CleanQueue()
-	/*
-		user := model.User{
-			TwitterIdStr: "140021552",
-			Name:         "otiai10",
-		}
-	*/
+	user := model.User{
+		TwitterIdStr: "140021552",
+		Name:         "otiai10",
+	}
+	user.Save()
 }
 
 func TestUser(t *testing.T) {
@@ -32,5 +31,27 @@ func TestEnqueue(t *testing.T) {
 }
 
 func TestFindUserByTwitterIdStr(t *testing.T) {
-	//
+	user, ok := model.FindUserByTwitterIdStr("140021552")
+	Expect(t, ok).ToBe(true)
+	Expect(t, user.Name).ToBe("otiai10")
+
+	user, ok = model.FindUserByTwitterIdStr("000")
+	Expect(t, ok).ToBe(false)
+	Expect(t, user.Name).ToBe("")
+}
+
+func TestCreateEventFromRequestParams(t *testing.T) {
+	event := model.CreateEventFromRequestParams(
+		10000000000000,
+		"This is message to be push-notified",
+		"遠征帰投",
+		"第",
+		"3",
+		"艦隊",
+		"mission-finish",
+		"38", "東京急行(弐)", "and", "any", "other", "optional", "strings",
+	)
+	Expect(t, event).TypeOf("model.Event")
+
+	Expect(t, len(event.OptionalStrings)).ToBe(7)
 }
